df158f22bb279cb67fcaa8102a4d4bb136bfc971
jokale <kalemajoanna@gmail.com>
2020-08-24 11:02:47 +0100

    create login component

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+class Login extends  React.Component {
+    render(){
+    return (
+               <div>
+                   <form>
+                       <input type='text'  id='email' name='email'></input>
+                        <label for ='email'>Email</label>
+                   </form>
+               </div> 
+            )
+
+    }
+        
+}
